d221a4858b40d7dcc40377256a0793aa35054ab7
two06 <two06@users.noreply.github.com>
2019-11-19 20:31:30 +0000

    Refactor to use block scanning

---
+++ AMSI_API/Controllers/AMSIScanLineBlocksController.cs
@@ -0,0 +1,41 @@
+﻿using AMSI_API.Models;
+using AMSI_Hooks.AntiMalwareScanInterface;
+using Scanners;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace AMSI_API.Controllers
+{
+    public class AMSIScanLineBlocksController : ApiController
+    {
+        public System.Web.Http.Results.JsonResult<List<BlockScanResult>> Post(int blockSize, HttpRequestMessage request)
+        {
+            var sample = request.Content.ReadAsStringAsync().Result;
+            var lines = sample.Split(new [] { Environment.NewLine}, StringSplitOptions.None);
+            var sub_sample = lines.Skip(0).Take(blockSize);
+            var result = new List<BlockScanResult>();
+            var scanner = new AntiMalwareScanInterface();
+            var block = 1;
+            while (sub_sample.Count() != 0)
+            {
+                var tempSample = string.Join("\r\n", sub_sample);
+                var tempSampleBytes = Encoding.UTF8.GetBytes(tempSample);
+                var scanResult = scanner.CallAntimalwareScanInterface(tempSampleBytes);
+                if (scanResult)
+                {
+                    result.Add(new BlockScanResult() {BlockNumber = block, BlockText = tempSample });
+                }
+                
+                sub_sample = lines.Skip(blockSize*block).Take(blockSize);
+                block++;
+            }
+           
+            return Json(result);
+        }
+    }
+}
---
+++ AMSI_API/Models/BlockScanResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AMSI_API.Models
+{
+    public class BlockScanResult
+    {
+        public int BlockNumber { get; set; }
+        public string BlockText { get; set; }
+    }
+}
